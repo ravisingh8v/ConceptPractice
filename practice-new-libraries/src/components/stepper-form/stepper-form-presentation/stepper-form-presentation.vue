72a6c1bd3793a0e1ecf55d4683733aca82f3b9ca
ravisingh8v <ravisingh8v@gmail.com>
2023-07-14 19:04:26 +0530

    feat: created stepper form

--- practice-new-libraries/src/components/stepper-form/stepper-form-presentation/stepper-form-presentation.vue
+++ practice-new-libraries/src/components/stepper-form/stepper-form-presentation/stepper-form-presentation.vue
@@ -22,16 +22,121 @@
                 :class="formIndex >= 2 ? 'bg-success' : ''"
               ></div>
             </div>
-            <!-- <activeComponent /> -->
+
             <Transition mode="out-in" name="stepper-form">
-              <component :is="activeComponent"></component>
+              <div v-if="formIndex === 1" key="personal-details">
+                <h2 class="text-center fw-bold text-secondary">
+                  Personal Details
+                </h2>
+                <div class="row mt-5">
+                  <div class="col-6">
+                    <Field
+                      type="text"
+                      id="firstname"
+                      name="firstName"
+                      class="form-control my-3"
+                      placeholder="Enter your firstname"
+                      v-model="formData.firstName"
+                      :rules="nameValidation"
+                    />
+                    <ErrorMessage
+                      name="firstName"
+                      as="div"
+                      class="error-message"
+                    ></ErrorMessage>
+                  </div>
+                  <div class="col-6">
+                    <Field
+                      type="text"
+                      id="lastname"
+                      name="lastName"
+                      class="form-control my-3"
+                      placeholder="Enter your lastname"
+                      v-model="formData.lastName"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      class="form-control my-3"
+                      placeholder="Enter your email"
+                      v-model="formData.email"
+                      :rules="emailValidation"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <Field
+                      type="text"
+                      id="phone"
+                      name="phone"
+                      class="form-control my-3"
+                      placeholder="Enter your phone"
+                      v-model="formData.phone"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div v-else-if="formIndex === 2" key="bank-details">
+                <h2 class="text-center fw-bold text-secondary">Bank Details</h2>
+                <div class="row mt-5">
+                  <div class="col-6">
+                    <Field
+                      name="accountNumber"
+                      type="text"
+                      id="accountNumber"
+                      class="form-control my-3"
+                      placeholder="Enter your account number"
+                      v-model="formData.accountNumber"
+                    />
+                  </div>
+                  <div class="col-6">
+                    <Field
+                      name="ifsc"
+                      type="text"
+                      id="ifsc"
+                      class="form-control my-3"
+                      placeholder="Enter your ifsc"
+                      v-model="formData.ifsc"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <Field
+                      name="cvv"
+                      type="email"
+                      id="cvv"
+                      class="form-control my-3"
+                      placeholder="Enter your cvv"
+                      v-model="formData.cvv"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <Field
+                      name="branch"
+                      type="text"
+                      id="branch"
+                      class="form-control my-3"
+                      placeholder="Enter your branch name"
+                      v-model="formData.branch"
+                    />
+                  </div>
+                </div>
+              </div>
             </Transition>
-            <!-- <stepperFormPersonalDetails /> -->
+
             <div class="row mt-5">
-              <!-- prev or cancel button  -->
               <div class="col-6">
                 <button
-                  v-if="formIndex == 1"
+                  v-if="formIndex === 1"
                   class="btn btn-secondary"
                   type="button"
                   @click="showConfirmation = true"
@@ -42,18 +147,17 @@
                   v-else
                   class="btn btn-secondary"
                   type="button"
-                  @click="formIndex = formIndex - 1"
+                  @click="prevStep"
                 >
                   previous
                 </button>
               </div>
-              <!-- next or submit -->
               <div class="col-6 text-end">
                 <button
-                  v-if="formIndex != 2"
+                  v-if="formIndex !== 2"
                   class="btn btn-success"
                   type="button"
-                  @click="getNextForm()"
+                  @click="nextStep"
                 >
                   next
                 </button>
@@ -61,12 +165,13 @@
                   v-else
                   class="btn btn-success"
                   type="submit"
-                  @click="getNextForm()"
+                  @click.prevent="submitForm"
                 >
                   Submit
                 </button>
               </div>
             </div>
+
             <Transition name="global">
               <div
                 v-if="showConfirmation"
@@ -75,50 +180,99 @@
             </Transition>
           </div>
         </form>
-        <!-- confirmation  -->
+
         <Transition name="global">
           <confirmationComponent
             v-if="showConfirmation"
             @onConfirm="onConfirm"
             @onCancel="onCancel"
-            :title="'are you sure, you want to exit?'"
+            :title="'Are you sure you want to exit?'"
           ></confirmationComponent>
         </Transition>
       </dialog>
     </Transition>
   </section>
 </template>
+
 <script setup lang="ts">
-import { ref, shallowRef, watch } from "vue";
+import { ref } from "vue";
 import confirmationComponent from "../confirmation-component.vue";
-import stepperFormPersonalDetails from "./../steppers-component/stepper-form-personal-details.vue";
-import stepperFormBankDetails from "./../steppers-component/stepper-form-bank-details.vue";
+import * as Yup from "yup";
+import { Field, ErrorMessage } from "vee-validate";
+
 const dialog = ref();
+
 function openDialog() {
   dialog.value.showModal();
 }
-// watch(dialog, () => {
-//   dialog.value?.addEventListener("click", (e) => {
-//     const modalDimension = dialog.value.getBoundingClientRect();
-//     if (
-//       e.clientX < modalDimension.left ||
-//       e.clientX > modalDimension.right ||
-//       e.clientY > modalDimension.top ||
-//       e.clientY < modalDimension.bottom
-//     ) {
-//       dialog.value.close();
-//     } else {
-//       return false;
-//     }
-//   });
-// });
+
+const formIndex = ref(1);
 const showConfirmation = ref(false);
+const formData = ref({
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  accountNumber: "",
+  ifsc: "",
+  cvv: "",
+  branch: "",
+});
+
+const nameValidation = Yup.string().required("Name is required");
+const emailValidation = Yup.string()
+  .email("Invalid email")
+  .required("Email is required");
+
+function nextStep() {
+  console.log("nexted");
+
+  const schema = Yup.object().shape({
+    firstName: nameValidation,
+    lastName: Yup.string(),
+    email: emailValidation,
+    phone: Yup.string(),
+    accountNumber: Yup.string(),
+    ifsc: Yup.string(),
+    cvv: Yup.string(),
+    branch: Yup.string(),
+  });
+
+  schema
+    .validate(formData.value)
+    .then(() => {
+      formIndex.value++;
+    })
+    .catch((errors) => {
+      console.error(errors);
+    });
+}
+
+function prevStep() {
+  formIndex.value--;
+}
+
+function submitForm() {
+  const schema = Yup.object().shape({
+    address: Yup.string().required("Address is required"),
+    phone: Yup.string().required("Phone number is required"),
+  });
+
+  schema
+    .validate(formData.value)
+    .then(() => {
+      console.log("Form submitted:", formData.value);
+      dialog.value.close();
+      formIndex.value = 1;
+    })
+    .catch((errors) => {
+      console.error(errors);
+    });
+  console.log(formData.value);
+}
 
 function onBackdropClick(e: any) {
   const modalDimension = dialog.value.getBoundingClientRect();
-  console.log(e.clientX);
-  console.log(modalDimension.top);
-  console.log(modalDimension.bottom);
 
   if (
     e.clientX < modalDimension.left ||
@@ -136,32 +290,14 @@ function onConfirm(value: any) {
     formIndex.value = 1;
   }
 }
+
 function onCancel(value: any) {
   if (value) {
     showConfirmation.value = false;
   }
 }
-
-// dynamic component
-const formIndex = ref(1);
-const activeComponent = shallowRef(stepperFormPersonalDetails);
-
-function getNextForm() {
-  if (formIndex.value < 2) formIndex.value = formIndex.value + 1;
-}
-
-watch(formIndex, () => {
-  if (formIndex.value == 1) {
-    activeComponent.value = stepperFormPersonalDetails;
-  }
-  if (formIndex.value == 2) {
-    activeComponent.value = stepperFormBankDetails;
-  }
-});
-
-// watch(showConfirmation, () => {
-// });
 </script>
+
 <style lang="scss">
 dialog::backdrop {
   background-color: rgba(0, 0, 0, 0.458);
--- practice-new-libraries/src/components/stepper-form/steppers-component/stepper-form-personal-details.vue
+++ practice-new-libraries/src/components/stepper-form/steppers-component/stepper-form-personal-details.vue
@@ -3,17 +3,19 @@
     <h2 class="text-center fw-bold text-secondary">Personal Details</h2>
     <div class="row mt-5">
       <div class="col-6">
-        <input
+        <Field
           type="text"
           id="firstname"
+          name="firstName"
           class="form-control my-3"
           placeholder="Enter your firstname"
         />
       </div>
       <div class="col-6">
-        <input
+        <Field
           type="text"
           id="lastname"
+          name="lastName"
           class="form-control my-3"
           placeholder="Enter your lastname"
         />
@@ -21,9 +23,10 @@
     </div>
     <div class="row">
       <div class="col-12">
-        <input
+        <Field
           type="email"
           id="email"
+          name="email"
           class="form-control my-3"
           placeholder="Enter your email"
         />
@@ -31,9 +34,10 @@
     </div>
     <div class="row">
       <div class="col-12">
-        <input
+        <Field
           type="text"
           id="phone"
+          name="phone"
           class="form-control my-3"
           placeholder="Enter your phone"
         />
@@ -41,3 +45,6 @@
     </div>
   </section>
 </template>
+<script setup lang="ts">
+import { Field } from "vee-validate";
+</script>
